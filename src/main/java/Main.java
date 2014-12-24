57250b68bc90cca1bf82a43a6fae9db3ca675e8b
sadv1r <sadv12@gmail.com>
2014-12-24 13:53:45 +0300

    Изменения в друзьях теперь заносятся в базу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Created by sadvr on 12/10/14.
@@ -14,7 +16,24 @@ public class Main {
         Users users = new Users();
 
         //while (true) {
-            users.compareFriends(dbFriends, parserFriends);
+            Map<Integer, Boolean> friendsChanges = users.compareFriends(dbFriends, parserFriends);
+            ArrayList<Integer> addedFriends = new ArrayList<>();
+            ArrayList<Integer> removedFriends = new ArrayList<>();
+
+            for (Entry<Integer, Boolean> entry : friendsChanges.entrySet()) {
+                if (entry.getValue()) {
+                    addedFriends.add(entry.getKey());
+                } else {
+                    removedFriends.add(entry.getKey());
+                }
+            }
+
+            if (addedFriends.size() != 0)
+                mysql.addFriends(Integer.parseInt(args[0]), addedFriends);
+
+            if (removedFriends.size() != 0)
+                mysql.removeFriends(Integer.parseInt(args[0]), removedFriends);
+
             /*try {
                 Thread.sleep(300000);
             } catch (InterruptedException e) {
