d04632ba3542e43729eac65bda66bba0e5cb8905
sadv1r <sadv12@gmail.com>
2014-12-24 13:53:30 +0300

    Метод compareFriends теперь возвращает изменения

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sadvr on 12/10/14.
@@ -6,14 +8,18 @@ import java.util.ArrayList;
 public class Users {
     // online: public Map<Integer, Boolean>
 
-    public void compareFriends(ArrayList<Integer> dbFriends, ArrayList<Integer> parserFriends) {
+    public Map<Integer, Boolean> compareFriends(ArrayList<Integer> dbFriends, ArrayList<Integer> parserFriends) {
+        Map<Integer, Boolean> friendsChanges = new HashMap<>();
         if (!parserFriends.equals(dbFriends)) {
             for (int a : parserFriends)
                 if (!dbFriends.contains(a))
-                    System.out.println("Новый друг " + a);
+                    //System.out.println("Новый друг " + a);
+                    friendsChanges.put(a, true);
             for (int a : dbFriends)
                 if (!parserFriends.contains(a))
-                    System.out.println("Удален друг " + a);
+                    //System.out.println("Удален друг " + a);
+                    friendsChanges.put(a, false);
         }
+        return friendsChanges;
     }
 }
