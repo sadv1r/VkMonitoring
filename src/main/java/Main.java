2e4648940558015b553eefd69fc767d2ec4e455a
sadv1r <sadv12@gmail.com>
2014-12-27 00:50:38 +0300

    Скрипт отрабатывает на друзьях пользователя

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,42 +10,21 @@ public class Main {
         Mysql mysql = new Mysql("localhost", "3306", "root", "", "vk_spy");
         Parser parser = new Parser();
 
-        ArrayList<Integer> dbFriends = mysql.getFriends(Integer.parseInt(args[0]));
-        ArrayList<Integer> parserFriends = parser.getFriends(Integer.parseInt(args[0]));
-
         Users users = new Users();
 
-        //while (true) {
-            Map<Integer, Boolean> friendsChanges = users.compareFriends(dbFriends, parserFriends);
-            ArrayList<Integer> addedFriends = new ArrayList<>();
-            ArrayList<Integer> removedFriends = new ArrayList<>();
+        while (true) {
+            ArrayList<Integer> friendsOfMainAccount = parser.getFriends(Integer.parseInt(args[0]));
+            for (int vkId : friendsOfMainAccount)
+                users.friendsMonitoring(vkId, mysql, parser);
 
-            for (Entry<Integer, Boolean> entry : friendsChanges.entrySet()) {
-                if (entry.getValue()) {
-                    addedFriends.add(entry.getKey());
-                } else {
-                    removedFriends.add(entry.getKey());
-                }
-            }
-
-            if (addedFriends.size() != 0)
-                mysql.addFriends(Integer.parseInt(args[0]), addedFriends);
-
-            if (removedFriends.size() != 0)
-                mysql.removeFriends(Integer.parseInt(args[0]), removedFriends);
-
-            /*try {
-                Thread.sleep(300000);
+            try {
+                Thread.sleep(60000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 break;
-            }*/
-        //}
+            }
+        }
 
     }
-
-
-
-
 
 }
--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -8,6 +8,30 @@ import java.util.Map;
 public class Users {
     // online: public Map<Integer, Boolean>
 
+    public void friendsMonitoring(int vkId, Mysql mysql, Parser parser) {
+        ArrayList<Integer> dbFriends = mysql.getFriends(vkId);
+        ArrayList<Integer> parserFriends = parser.getFriends(vkId);
+
+        Map<Integer, Boolean> friendsChanges = compareFriends(dbFriends, parserFriends);
+
+        ArrayList<Integer> addedFriends = new ArrayList<>();
+        ArrayList<Integer> removedFriends = new ArrayList<>();
+
+        for (Map.Entry<Integer, Boolean> entry : friendsChanges.entrySet()) {
+            if (entry.getValue()) {
+                addedFriends.add(entry.getKey());
+            } else {
+                removedFriends.add(entry.getKey());
+            }
+        }
+
+        if (addedFriends.size() != 0)
+            mysql.addFriends(vkId, addedFriends);
+
+        if (removedFriends.size() != 0)
+            mysql.removeFriends(vkId, removedFriends);
+    }
+
     public Map<Integer, Boolean> compareFriends(ArrayList<Integer> dbFriends, ArrayList<Integer> parserFriends) {
         Map<Integer, Boolean> friendsChanges = new HashMap<>();
         if (!parserFriends.equals(dbFriends)) {
