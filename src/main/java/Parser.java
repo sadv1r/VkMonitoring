27fad4355a697f76680b09a570e7d389e63aa96d
sadv1r <sadv12@gmail.com>
2015-01-04 23:48:17 +0300

    Метод getOnline добавлен и реализован

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -13,6 +13,35 @@ import java.util.Scanner;
 public class Parser {
     private static final Logger LOGGER = Logger.getLogger(Parser.class);
 
+    public ArrayList<int[]> getOnline(String vkIds) {
+        LOGGER.debug("Метод getOnline запущен");
+        ArrayList<int[]> onlineInfo = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        String friendsString = getStringFromApi("https://api.vk.com/method/users.get?v=5.24&fields=online,online_mobile,last_seen&user_ids=" + vkIds);
+        try {
+            JsonNode rootNode = mapper.readTree(friendsString).get("response");
+            LOGGER.trace("\"items\" из \"response\" получены успешно");
+
+            if (rootNode != null)
+                for (JsonNode node : rootNode) {
+                    int id = node.get("id").asInt();
+                    int online = node.get("online").asInt();
+                    int time = node.get("last_seen").get("time").asInt();
+                    int platform = node.get("last_seen").get("platform").asInt();
+                    onlineInfo.add(new int[]{id, online, time, platform});
+                }
+
+            LOGGER.info("Получена информация о " + onlineInfo.size() + " пользователях");
+
+        } catch (IOException e) {
+            LOGGER.error(e);
+        }
+
+        LOGGER.debug("Метод getOnline завершил работу");
+        return onlineInfo;
+    }
+
     public ArrayList<Integer> getFriends(int vkId) {
         LOGGER.debug("Метод getFriends запущен");
         ArrayList<Integer> friends = new ArrayList<>();
